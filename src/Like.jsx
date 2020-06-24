58557601ef1deecb6e6626a375b1c0d06e2ab0c4
Malik HARRIZ <malik.h@webdevpro.net>
2020-06-24 16:40:20 +0200

    stateless fonctionnal Composant

--- src/Like.jsx
+++ src/Like.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class Like extends Component {
+/* class Like extends Component {
 
     render() {
         return (
@@ -14,4 +14,24 @@ class Like extends Component {
     }
 }
 
+export default Like;*/
+// Fonction Stateless => sfc 
+
+const Like = (props) => {
+    return (<div>
+        Like
+        <span className="badge badge-danger m-2">{props.like.valeur}</span>
+        <button className="btn btn-info m-2" onClick={() => { props.augmenter(props.like) }}>+</button>
+        <button className="btn btn-danger m-2" onClick={() => { props.diminuer(props.like) }}>-</button>
+    </div>);
+}
+
 export default Like;
+// si un composant ne dispose pas de données propres (state) => le mieux est de créer une fonction
+// Like sans la propriété state => Composant Stateless
+// render => affichage du composant
+// aucun donne stockée 
+// récupérer des données depuis son parent 
+// il est possible de créer le composant d'une autre manière
+// cc => Class
+// sfc => Fonction 
